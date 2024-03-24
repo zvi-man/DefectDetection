21b82fc4c1c2563d21782dfcbb04e0b42750ac03
ZviLinux <zvymendel@gmail.com>
2024-03-24 22:44:55 +0200

    Added augmentations to synthetic_dataset example

--- synthetic_data_creation/synthetic_dataset.py
+++ synthetic_data_creation/synthetic_dataset.py
@@ -3,7 +3,8 @@ from typing import Optional
 import numpy as np
 import torch
 from torch.utils.data import Dataset
-import torchvision.transforms as transforms
+
+from ml_method.transform import get_image_and_mask_transforms
 from synthetic_data_creation.add_defect import add_random_defect
 from utilities.display_utils import DisplayUtils
 from utilities.general_utils import GeneralUtils
@@ -25,12 +26,12 @@ class DefectDataset(Dataset):
 
     def __getitem__(self, idx):
         # Generate a new image with defects
-        augmented_image, defect_mask = add_random_defect(self.image)
+        sample = add_random_defect(self.image)
 
         if self.transform is not None:
-            augmented_image = self.transform(augmented_image)
+            sample = self.transform(sample)
 
-        return augmented_image, defect_mask
+        return sample
 
 
 def example():
@@ -40,12 +41,19 @@ def example():
         tiff_image_path=reference_img_path,
         to_display=True)
     # Create the dataset
-    dataset = DefectDataset(reference_im, num_images=10)
+    # dataset = DefectDataset(reference_im, num_images=10, transform=RandomShiftWithMask(0.1))
+    dataset = DefectDataset(reference_im, num_images=10, transform=get_image_and_mask_transforms())
     # Example of iterating through the dataset
     for i in range(len(dataset)):
         augmented_image, defect_mask = dataset[i]
         # Display the image
-        DisplayUtils.display_2_images_side_by_side(augmented_image, defect_mask * 255, f"Augmented image {i}")
+        if isinstance(augmented_image, torch.Tensor):
+            im_to_display = augmented_image[0, :, :].squeeze(0).numpy()
+            im_to_display = (im_to_display - im_to_display.min()) / (im_to_display.max() - im_to_display.min())
+            mask_to_display = defect_mask.squeeze(0).numpy()
+            DisplayUtils.display_2_images_side_by_side(im_to_display, mask_to_display, f"Augmented image {i}")
+        else:
+            DisplayUtils.display_2_images_side_by_side(augmented_image, defect_mask, f"Augmented image {i}")
 
 
 # Example usage
